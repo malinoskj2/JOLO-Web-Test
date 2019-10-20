4b03a65234887f465abe8f2dc9b163a463fc9e19
EFliegel <fliege39@students.rowan.edu>
2019-10-20 19:16:15 -0400

    Add files via upload

---
+++ ExaminerRepository.java
@@ -0,0 +1,6 @@
+package srproject.jolo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import srproject.jolo.controllers.Examiner;
+
+public interface ExaminerRepository extends CrudRepository<Examiner, Long> { }
---
+++ JoloApplication.java
@@ -0,0 +1,13 @@
+package srproject.jolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoloApplication.class, args);
+	}
+
+}
