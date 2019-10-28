729529a887657a1e3f59e2372aaed05e169c637b
Aidan Melchior <aidan.melchior@yahoo.com>
2019-10-28 01:59:08 -0400

    Created AudioResult object which formats the data into the /get_result output format

--- server/src/main/java/server/controller/AudioController.java
+++ server/src/main/java/server/controller/AudioController.java
@@ -9,23 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
 import java.util.Arrays;
 import server.model.AudioResult;
+import server.repository.AudioRepository;
 import server.repository.remoteAPI;
 
 
-//localhost8080/get_result
-@RequestMapping("/get_result")
+//localhost:8080/get_result
+
 public class AudioController {
 
     private FileSystemResource fsr;
     //AudioResult ar = new AudioResult();
     private String output;
+    private AudioRepository audioRepository = new AudioRepository();
 
-    public String voice(@RequestParam(value="content", defaultValue="") String content) {
+    @RequestMapping("/get_result")
+    public String voice_to_text(@RequestParam(value="content", defaultValue="") String content) {
+        System.out.println("0");
+
         try {
+            System.out.println("1");
             String api = remoteAPI.api;
-            //FileSystemResource fsr = vr.getFileSystemResource();
+            audioRepository.add_test_file();
+            FileSystemResource fsr = audioRepository.getFsr_list().getLast();      //access audio database
+
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
             headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
@@ -44,5 +53,13 @@ public class AudioController {
             ex.printStackTrace();
             throw (ex);
         }
+    }
+
+    public String get_result() {
+
+        System.out.println("2");
+
+        //Json out:{ Number:testId, Number:questionID, Number:GuessedAngle1, Number:GuessedAngle2, Number:correctGuess, String: audioFile}
+        return voice_to_text("");
     }
 }
--- server/src/main/java/server/model/AudioResult.java
+++ server/src/main/java/server/model/AudioResult.java
@@ -1,4 +1,62 @@
 package server.model;
 
+import org.springframework.core.io.FileSystemResource;
+import server.repository.AudioRepository;
+
+import java.util.regex.Pattern;
+
 public class AudioResult {
+
+    private FileSystemResource fsr;
+    //AudioResult ar = new AudioResult();
+    private String content;
+    private AudioRepository audioRepository;
+    private int testID;
+    private int questionId;
+    private int GuessedAngle0;
+    private int GuessedAngle1;
+    private int correctGuess;
+    private String audioFile;
+    //Json out:{ Number:testId, Number:questionID, Number:GuessedAngle1, Number:GuessedAngle2, Number:correctGuess, String: audioFile}
+    public AudioResult(String audioOutput) {
+        content = audioOutput;
+        audioRepository = new AudioRepository();
+        testID = 0;
+        questionId = 0;
+        GuessedAngle0 = getAngle(0);
+        GuessedAngle1 = getAngle(1);
+        correctGuess = 0;
+        audioFile = "";
+    }
+
+    private int getAngle(int index) {
+        String temp = content.substring(1, content.length() - 1);     //[["A",x,y],["B",x,y]] -> ["A",x,y],["B",x,y]
+        String[] words = temp.split(Pattern.quote("],"));                   //["A",x,y   ["B",x,y]
+        String[] newwords = new String[2];
+        int i = 0;
+        for (String word : words) {
+            String[] s = word.split(",");                       //"A"    "B"
+            word = s[0].substring(2,s[0].length()-1);                 // A
+            newwords[i] = word;
+            i++;
+        }
+        String word_at_index = newwords[index];
+        int guessed_angle;
+        switch(word_at_index){
+            case "zero":
+                guessed_angle = 0;
+                break;
+            case "one":
+                guessed_angle = 1;
+                break;
+                //...
+
+            default:
+                guessed_angle = -1;
+
+        }
+
+
+        return guessed_angle;
+    }
 }
