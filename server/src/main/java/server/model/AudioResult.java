ab1c42791be9252d2dac0f6c6aa2f24f66ed1fbf
Aidan Melchior <aidan.melchior@yahoo.com>
2019-10-30 01:58:58 -0400

    completed necessary audioResult methods implemented AudioRepository AudioRepository is a simple Linked List temporarily

--- server/src/main/java/server/model/AudioResult.java
+++ server/src/main/java/server/model/AudioResult.java
@@ -3,6 +3,7 @@ package server.model;
 import org.springframework.core.io.FileSystemResource;
 import server.repository.AudioRepository;
 
+import java.io.File;
 import java.util.regex.Pattern;
 
 public class AudioResult {
@@ -17,16 +18,27 @@ public class AudioResult {
     private int GuessedAngle1;
     private int correctGuess;
     private String audioFile;
-    //Json out:{ Number:testId, Number:questionID, Number:GuessedAngle1, Number:GuessedAngle2, Number:correctGuess, String: audioFile}
-    public AudioResult(String audioOutput) {
-        content = audioOutput;
+
+    public AudioResult() {
+        content = "";
         audioRepository = new AudioRepository();
+        FileSystemResource fsr = new FileSystemResource(
+                new File(System.getProperty("user.dir") + "\\"+ "resources\\B64DJ1.wav"));
+        audioRepository.add(fsr);
         testID = 0;
         questionId = 0;
         GuessedAngle0 = getAngle(0);
         GuessedAngle1 = getAngle(1);
         correctGuess = 0;
         audioFile = "";
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public FileSystemResource get_latest_fsr() {
+        return audioRepository.getLast();
     }
 
     private int getAngle(int index) {
@@ -49,8 +61,30 @@ public class AudioResult {
             case "one":
                 guessed_angle = 1;
                 break;
-                //...
-
+            case "two":
+                guessed_angle = 2;
+                break;
+            case "three":
+                guessed_angle = 3;
+                break;
+            case "four":
+                guessed_angle = 4;
+                break;
+            case "five":
+                guessed_angle = 5;
+                break;
+            case "six":
+                guessed_angle = 6;
+                break;
+            case "seven":
+                guessed_angle = 7;
+                break;
+            case "eight":
+                guessed_angle = 8;
+                break;
+            case "nine":
+                guessed_angle = 9;
+                break;
             default:
                 guessed_angle = -1;
 
@@ -58,5 +92,13 @@ public class AudioResult {
 
 
         return guessed_angle;
+    }
+
+    public String format() {
+        //Json out:{ Number:testId, Number:questionID, Number:GuessedAngle1, Number:GuessedAngle2, Number:correctGuess, String: audioFile}
+
+        return "{Number:" +testID + ",Number:" + questionId + ",Number:" + getAngle(0) +
+                ",Number:" + getAngle(1) + ",Number:" + correctGuess +
+                ",String:" + audioRepository.getLast().getFilename() + "}";
     }
 }
--- server/src/main/java/server/repository/AudioRepository.java
+++ server/src/main/java/server/repository/AudioRepository.java
@@ -1,26 +1,6 @@
 package server.repository;
 
 import org.springframework.core.io.FileSystemResource;
-
-import java.io.File;
 import java.util.LinkedList;
-import java.util.List;
 
-public class AudioRepository {
-    private LinkedList<FileSystemResource> fsr_list = new LinkedList<>();
-
-
-
-    public void add_test_file() {
-        FileSystemResource fsr = new FileSystemResource(
-                new File(System.getProperty("user.dir") + "\\"+ "resources\\B64DJ1.wav"));
-        fsr_list.add(fsr);
-    }
-
-    public LinkedList<FileSystemResource> getFsr_list() {
-        return fsr_list;
-    }
-
-    //(new File
-            //(System.getProperty("user.dir") + "\\"+ "B64DJ1.wav"));
-}
+public class AudioRepository extends LinkedList<FileSystemResource> { }
