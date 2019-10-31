64e4f387565817ec9312fad8e5e9d02bfdd5eac9
Justin Serrano <j.serrano331@gmail.com>
2019-10-31 12:40:39 -0400

    Unfinished SignUp and Draw

---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,103 @@
+<template>
+<div id="signup">
+    <v-app-bar dense>
+            <v-toolbar-title color="">Angle-Matching Web Test</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn> Home </v-btn>
+                <v-btn> About </v-btn>
+                <v-btn> Login </v-btn>
+            </v-toolbar-items>
+      </v-app-bar>
+  <v-content>
+      <v-container>
+         <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+         <v-card>
+             <v-flex>
+             <v-card-title class="justify-center">Sign Up</v-card-title>
+             </v-flex>
+          <v-card-text>
+          <v-form>
+              <v-text-field
+                v-model="fName"
+                label="First Name"
+                name="firstName"
+                type="text"
+                required
+                />
+                 <v-text-field
+                v-model="lName"
+                label="Last Name"
+                name="lastName"
+                type="text"
+                required
+                />
+                 <v-text-field
+                v-model="Email"
+                label="Email"
+                name="email"
+                type="email"
+                required
+                />
+                 <v-text-field
+                 v-model="Password"
+                label="Password"
+                name="password"
+                type="password"
+                required
+                />
+                <v-text-field
+                v-model="retypePassword"
+                label="Retype Password"
+                name="retypePassword"
+                type="password"
+                required
+                />
+            </v-form>
+          </v-card-text>
+          <v-card-actions >
+                <v-spacer />
+                <v-flex>
+                <v-btn class="justify-center" color="primary" @click="submit()">Submit</v-btn>
+                </v-flex>
+              </v-card-actions>
+         </v-card>
+          </v-col>
+         </v-row>
+      </v-container>
+  </v-content>
+</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    fName: '',
+    lName: '',
+    Email: '',
+    Password: '',
+    retypePassword: '',
+  }),
+  methods: {
+    submit() {
+      console.log(`${this.fName}\n${
+        this.lName}\n${
+        this.Email}\n${
+        this.Password}\n${
+        this.retypePassword}`);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
