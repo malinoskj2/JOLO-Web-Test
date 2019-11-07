d31fb3377b191233eeeba9bad304f31362e86c14
Saint-Somnia <efliegel58@gmail.com>
2019-11-07 17:51:57 -0500

    authentication

--- server/src/main/java/server/model/request/AuthenticationRequest.java
+++ server/src/main/java/server/model/request/AuthenticationRequest.java
@@ -3,19 +3,34 @@ package server.model.request;
 import java.io.Serializable;
 
 public class AuthenticationRequest implements Serializable {
-    private final String email;
-    private final String password;
+	private String email;
+	private String password;
+	public AuthenticationRequest() {}
+	public AuthenticationRequest(String username, String password) {
+    	this.email = username;
+    	this.password = password;
 
-    public AuthenticationRequest(String username, String password) {
-        this.email = username;
-        this.password = password;
     }
 
     public String getEmail() {
-        return email;
+    	return email;
+
     }
+
     public String getPassword() {
-        return password;
+    	return password;
+
     }
+
+    public void setEmail(String email) {
+    	this.email = email;
+
+    }
+
+    public void setPassword(String password) {
+    	this.password = password;
+
+    }
+
 }
 
