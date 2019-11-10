1d5fa1daa1bc7c7f5a4803986e94280964dfe56e
ebarnes27 <barnese0@students.rowan.edu>
2019-11-10 11:08:08 -0500

    vuetify code for the buttons

---
+++ buttons.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>This is the test homepage</h1>
+
+    <v-btn class="red white--">Record Answer and Stop Time</v-btn>
+
+    <v-btn class="red white--">Continue to Next Question</v-btn>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
