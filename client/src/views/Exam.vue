85d6835e692dcca36522c9b526738f1533897811
Jesse Malinosky <jesse@malinoskj2.dev>
2019-11-16 16:53:31 +0000

    add exam exit guard (#22)

--- client/src/views/Exam.vue
+++ client/src/views/Exam.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="my-auto mx-auto">
       <patient-i-d-prompt @set-patient="start"/>
+      <v-dialog v-model="exitGuard" persistent max-width="290">
+        <v-card>
+          <v-card-title class="headline">Quitting The Exam?</v-card-title>
+          <v-card-text>Only completed trials will be recorded.</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="exitGuard = false">No</v-btn>
+            <v-btn color="primary" text @click="next">Yes</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <DrawTest :questions="test.questions"
                 :testSubmissionID="test.testSubmissionID"/>
       <v-spacer></v-spacer>
@@ -14,6 +25,11 @@ import PatientIDPrompt from '@/components/PatientIDPrompt.vue';
 
 export default {
   name: 'exam',
+  // eslint-disable-next-line no-unused-vars
+  beforeRouteLeave(to, from, next) {
+    this.next = next;
+    this.exitGuard = true;
+  },
   components: {
     DrawTest,
     PatientIDPrompt,
@@ -24,6 +40,8 @@ export default {
         questions: [],
         testSubmissionID: 0,
       },
+      exitGuard: false,
+      next: {},
     };
   },
   methods: {
