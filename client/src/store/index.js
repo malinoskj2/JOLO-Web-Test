468449754d3a01b54ac55663f063867f3828668f
Jesse Malinosky <jesse@malinoskj2.dev>
2019-11-18 09:33:33 -0500

    make user module

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -10,10 +10,6 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    token: null,
-    email: null,
-    firstName: null,
-    lastName: null,
     test: {
       questions: [],
       testSubmissionID: 0,
@@ -21,22 +17,6 @@ export default new Vuex.Store({
     patients: [],
   },
   mutations: {
-    saveToken(state, token) {
-      state.token = token;
-    },
-    saveEmail(state, email) {
-      state.email = email;
-    },
-    saveFirstName(state, firstName) {
-      state.firstName = firstName;
-    },
-    saveLastName(state, lastName) {
-      state.lastName = lastName;
-    },
-    signOut(state) {
-      state.email = null;
-      state.token = null;
-    },
     saveTest(state, test) {
       state.test = test;
     },
@@ -45,11 +25,6 @@ export default new Vuex.Store({
     },
   },
   getters: {
-    token: state => state.token,
-    email: state => state.email,
-    firstName: state => state.firstName,
-    lastName: state => state.lastName,
-    isAuthenticated: state => Boolean(state.token),
     test: state => state.test,
     patients: state => state.patients,
   },
@@ -68,42 +43,6 @@ export default new Vuex.Store({
       });
 
       context.commit('saveTest', await response.json());
-    },
-    async login(context, payload) {
-      const response = await fetch(`${process.env.VUE_APP_API}/auth/authenticate`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-        }),
-      });
-
-      const json = await response.json();
-      context.commit('saveEmail', json.email);
-      context.commit('saveToken', json.token);
-      context.commit('saveFirstName', json.firstName);
-      context.commit('saveLastName', json.lastName);
-    },
-    async signup(context, payload) {
-      const response = await fetch(`${process.env.VUE_APP_API}/auth/register`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          fName: payload.fName,
-          lName: payload.lName,
-          password: payload.password,
-          email: payload.email,
-        }),
-      });
-
-      if (response.ok) {
-        payload.successFunction();
-      }
     },
     async fetchPatients(context) {
       const response = await fetch(`${process.env.VUE_APP_API}/patient/all`, {
