661092f68b25b1f94352420023ee8069bbc885ca
Justin Serrano <j.serrano331@gmail.com>
2019-11-25 23:53:25 -0500

    New bottom array + numbered

--- client/src/views/Exam.vue
+++ client/src/views/Exam.vue
@@ -15,7 +15,6 @@
       <DrawTest :questions="test.questions"
                 :testSubmissionID="test.testSubmissionID"/>
       <v-spacer></v-spacer>
-      <canvas id="canvas2"></canvas>
     </div>
 </template>
 
@@ -58,52 +57,7 @@ export default {
       })
         .then(() => console.log('fetched test'))
         .catch(() => console.log('error fetching test'));
-      this.draw();
       this.$store.commit('setInProgress');
-    },
-    draw() {
-      const canvas = document.getElementById('canvas2');
-      if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
-        ctx.canvas.width = 400;
-        ctx.canvas.height = 400;
-        ctx.beginPath();
-        ctx.strokeStyle = 'black';
-        // Line 1
-        ctx.moveTo(170, 200);
-        ctx.lineTo(80, 200);
-        // // Line 2
-        ctx.moveTo(172, 192);
-        ctx.lineTo(82, 170);
-        // // Line 3
-        ctx.moveTo(176, 186);
-        ctx.lineTo(96, 138);
-        // // Line 4
-        ctx.moveTo(180, 182);
-        ctx.lineTo(116, 114);
-        // // Line 5
-        ctx.moveTo(186, 178);
-        ctx.lineTo(152, 98);
-        // // Line 6
-        ctx.moveTo(194, 178);
-        ctx.lineTo(194, 90);
-        // // Line 7
-        ctx.moveTo(200, 178);
-        ctx.lineTo(232, 98);
-        // // Line 8
-        ctx.moveTo(208, 182);
-        ctx.lineTo(266, 114);
-        // // Line 9
-        ctx.moveTo(212, 186);
-        ctx.lineTo(286, 138);
-        // // Line 10
-        ctx.moveTo(214, 192);
-        ctx.lineTo(300, 170);
-        // Line 11
-        ctx.moveTo(216, 200);
-        ctx.lineTo(310, 200);
-        ctx.stroke();
-      }
     },
   },
   computed: {
