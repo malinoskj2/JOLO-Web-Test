ebfabf4f4c7db18008082e024a2f4676a4e3fb05
Jesse Malinosky <jesse@malinoskj2.dev>
2019-11-27 08:05:26 -0500

    add spreadsheet dl

--- client/src/components/PatientList.vue
+++ client/src/components/PatientList.vue
@@ -2,18 +2,29 @@
   <v-container>
     <v-data-table
       :headers="headers"
-      :items="$store.getters.patients"
-      :items-per-page="15"
-      class="elevation-1"/>
+      :items="patients"
+      :items-per-page="itemsPerPage"
+      class="elevation-1">
+
+      <template v-slot:item.download="{ item }">
+
+          <v-icon @click="downloadSpreadSheet(item.ID)">
+            {{fileDownloadSvgPath}}
+          </v-icon>
+      </template>
+    </v-data-table>
   </v-container>
 
 </template>
 
 <script>
+import { mdiFileDownload } from '@mdi/js';
+
 export default {
   name: 'GetResults',
   data() {
     return {
+      fileDownloadSvgPath: mdiFileDownload,
       headers: [
         {
           text: 'Patient ID',
@@ -29,7 +40,39 @@ export default {
         },
       ],
       patientData: [],
+      itemsPerPage: 15,
     };
+  },
+  computed: {
+    patients() {
+      return this.$store.getters.patients
+        .map(patient => ({ download: this.fileDownloadSvgPath, ...patient }));
+    },
+  },
+  methods: {
+    downloadSpreadSheet(patientID) {
+      console.log(patientID);
+
+      fetch(`${process.env.VUE_APP_API}/patient/spreadsheet`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${this.$store.getters.token}`,
+        },
+      }).then(resp => resp.blob())
+        .then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = url;
+          // the filename you want
+          a.download = 'todo-1.xls';
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+          alert('your file has downloaded!');
+        })
+        .catch(() => console.log('Could not download the spreadsheet.'));
+    },
   },
   mounted() {
     this.$store.dispatch('makeAuthenticatedCall', {
